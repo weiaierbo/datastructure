0156b0a10dba10f52f860a295abe992947bdc275
xukai <xukai@zj.tech>
2021-07-16 20:30:11 +0800

    插值查找

--- src/main/java/com/xk/search/BinarySearch.java
+++ src/main/java/com/xk/search/BinarySearch.java
@@ -7,7 +7,7 @@ package com.xk.search;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr={1,3,4,6,7,8};
-        int search = binarySearch(arr, 2);
+        int search = binarySearch(arr, 300);
         if(search < 0){
             System.out.println("未找到");
         } else {
@@ -22,11 +22,18 @@ public class BinarySearch {
      * @return
      */
     public static int binarySearch(int[] arr,int value){
-
+        //对于插值查找，必须需要，否则下面的mid可能越界
+        if(value < arr[0] || value > arr[arr.length-1]){
+            System.out.println("不存在");
+            return -1;
+        }
         int left = 0;
         int right = arr.length-1;
         while(left <= right){
-            int mid = (left+right)/2;
+            // 普通查找
+            //int mid = (left+right)/2;
+            // 插值查找，适合关键字分布均匀的情况
+            int mid = left+((value-arr[left])/(arr[right]-arr[left]))*(right-left);
             if(arr[mid] == value){
                 return mid;
             }
